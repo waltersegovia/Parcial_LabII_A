168194eec429af905fe274b0ec6f61f1150b0c47
waltersegovia <wseg@hotmail.com>
2020-05-12 13:55:44 -0300

    PP Lab II

---
+++ SegoviaWalter_PP2D/Jardin/FrmBuscarPorApellido.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Jardin
+{
+    partial class FrmBuscarPorApellido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstApellidos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstApellidos
+            // 
+            this.lstApellidos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lstApellidos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lstApellidos.HideSelection = false;
+            this.lstApellidos.Location = new System.Drawing.Point(0, 41);
+            this.lstApellidos.Name = "lstApellidos";
+            this.lstApellidos.Size = new System.Drawing.Size(434, 359);
+            this.lstApellidos.TabIndex = 0;
+            this.lstApellidos.UseCompatibleStateImageBehavior = false;
+            this.lstApellidos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Apellido";
+            this.columnHeader1.Width = 93;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nombre";
+            this.columnHeader2.Width = 108;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Legajo";
+            this.columnHeader3.Width = 63;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Sala";
+            this.columnHeader4.Width = 73;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Cuota";
+            this.columnHeader5.Width = 104;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(12, 12);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(74, 9);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(115, 20);
+            this.txtApellido.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(349, 7);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // FrmBuscarPorApellido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 412);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lstApellidos);
+            this.Name = "FrmBuscarPorApellido";
+            this.Text = "Busqueda po Apellido";
+            this.Load += new System.EventHandler(this.FrmBuscarPorApellido_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstApellidos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
---
+++ SegoviaWalter_PP2D/Jardin/FrmBuscarPorApellido.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace Jardin
+{
+    public partial class FrmBuscarPorApellido : Form
+    {
+        private List<Alumno> listaPorApellidos;
+        private List<Alumno> lista2;
+        string apellido;
+
+        public FrmBuscarPorApellido(List<Alumno> listaAlumnos)
+        {
+            InitializeComponent();
+
+            this.listaPorApellidos = listaAlumnos;
+        }
+
+        //public List<Alumno> ListaPorApellidos { get { return this.listaPorApellidos; } }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            apellido = txtApellido.Text;
+            int cont = 0;
+            foreach (Alumno item in listaPorApellidos)
+            {
+                if (item.Apellido == apellido)
+                {
+                    cont++;
+                    lista2.Add(item);
+                }
+            }
+
+            if (cont == 0)
+                MessageBox.Show("No se encontraron resultados");
+            else
+            {
+                foreach (Alumno item in listaPorApellidos)
+                {
+                    ListViewItem lista = new ListViewItem(item.Apellido);
+                    lista.SubItems.Add(item.Nombre);
+                    lista.SubItems.Add(item.Legajo.ToString());
+                    lista.SubItems.Add(item.ColorSala.ToString());
+                    lista.SubItems.Add("$" + item.PrecioCuota.ToString());
+
+                    lstApellidos.Items.Add(lista);
+                }
+            }
+        }
+
+        private void FrmBuscarPorApellido_Load(object sender, EventArgs e)
+        {
+            lista2 = new List<Alumno>();
+        }
+    }
+}
